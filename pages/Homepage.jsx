8709a710cbcbd85f79e4b62e548e4e31335b0e70
Onkar Patil <102043839+Foxhog-x@users.noreply.github.com>
2024-01-11 23:33:25 +0530

    initial init

---
+++ pages/Homepage.jsx
@@ -0,0 +1,96 @@
+export const Homepage = () => {
+  return (
+    <>
+      <div className="maincontainer">
+        <div className="nav_bar">
+          <div className="nav_bar_link">
+            <div className="nav_links_top">
+              <span>
+                <input type="text" placeholder="search" />
+              </span>
+              <span>Download</span>
+              <span>Investor</span>
+              <span>Carrers</span>
+              <span>Contact</span>
+              <span>Portals</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="nav_content">
+        <div className="nav_bar_drop">
+          <div className="logo">
+            <h1>Logo</h1>
+          </div>
+          <div className="nav_link">
+            <span>Home</span>
+            <span>About</span>
+            <span>Segment</span>
+            <span>Die Shop</span>
+            <span>Technology</span>
+          </div>
+        </div>
+      </div>
+      <section>
+        <div className="nav_bar_bottom">
+          <div className="nav_bar_bottom_links">
+            <span>Home></span>
+            <span>About></span>
+            <span>Company></span>
+          </div>
+        </div>
+        <div className="heading_container">
+          <div className="heading_tag">
+            <h1>WELCOME TO RELIABLE</h1>
+            <div className="heading_tag_bottom">_____________</div>
+            <section>
+              <div className="section_content">
+                <p>
+                  RELIABLE is a Robust and Efficient organization, Leading in
+                  Innovation with Technological Advancements, which is
+                  Beneficial and Lucrative for our clients; thus, helping us
+                  build Enduring relationships.
+                </p>
+                <p>
+                  We provide end-to-end solutions starting from designing,
+                  prototyping and development of dies, fixtures, and gauges,
+                  stamping components, setting up welded assembly cells/lines,
+                  powder coating or E-coating, remote warehousing and supplying
+                  to OEMs in the domestic and overseas markets through an
+                  efficient "Just-In-Time" supply chain.
+                </p>
+              </div>
+            </section>
+          </div>
+        </div>
+        <div className="about_us_container">
+          <div className="about_us_content">
+            <span className="about_us_text">_____________</span>
+            <h1 className="about_us_text">ABOUT US</h1>
+            <div className="about_section">
+              <p className="about_us_text">
+                Reliable Autotech Pvt. Ltd., is a globally renowned sheet
+                metal-based engineering and manufacturing company with a team of
+                skilled professionals, state-of-the-art infrastructure &
+                globally recognized processes. As a result of our diverse skills
+                in production, procurement, and process management along with
+                our commitment to on-time delivery and superior quality,
+                Reliable has become one of the preferred world players in the
+                engineering and mobility industry.
+              </p>
+              <button className="watch_about_btn">Watch capture Video</button>
+            </div>
+          </div>
+        </div>
+        <div className="nav_bar"></div>
+        <div className="corporate_philosophy_container">
+          <div className="philosopy_content">
+            <span>___________</span>
+            <h1>CORPORATE PHILOSOPHY</h1>
+          </div>
+          <div>hello</div>
+        </div>
+      </section>
+    </>
+  );
+};
