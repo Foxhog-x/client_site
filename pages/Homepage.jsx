0b7413ea885f4a0e7f13f9dae2ac43bbf7b5c7d9
Onkar Patil <102043839+Foxhog-x@users.noreply.github.com>
2024-01-28 14:42:36 +0530

    init

--- pages/Homepage.jsx
+++ pages/Homepage.jsx
@@ -83,14 +83,291 @@ export const Homepage = () => {
           </div>
         </div>
         <div className="nav_bar"></div>
-        <div className="corporate_philosophy_container">
-          <div className="philosopy_content">
-            <span>___________</span>
-            <h1>CORPORATE PHILOSOPHY</h1>
+        <div>
+          <div className="corporate_philosophy_container">
+            <div className="philosopy_content">
+              <span>___________</span>
+              <h1>CORPORATE PHILOSOPHY</h1>
+            </div>
+            <div className="phi_container">
+              <div className="phi_container_inside">
+                <div className="col">
+                  <span>Our Vision</span>
+                  <p>
+                    We shall be a world-class engineering company operating in
+                    multiple domains serving the needs of the customers in
+                    diverse sectors to achieve sustainable multi-fold growth.
+                  </p>
+                </div>
+                <div className="col">
+                  <span>Our Mission</span>
+                  <p>
+                    Our mission is to provide exceptional value to our customers
+                    by being a total solution provider in engineering products &
+                    services.
+                  </p>
+                  <p>
+                    We are committed to long term sustainable growth by adopting
+                    world-class practices embedded in total quality culture.
+                  </p>
+                  <p>
+                    We believe in being responsible towards our employees and
+                    the communities in which we operate.
+                  </p>
+                </div>
+                <div className="col">
+                  <span>Core Values</span>
+                  <p>
+                    The culture of any Company is largely defined by the Core
+                    Values it adopts and lives by. We at Reliable live by 4 Core
+                    Values -
+                    <ul className="list_core_value">
+                      <li>Employee Success</li>
+                      <li>Respect & Trust</li>
+                      <li>Customer Focus</li>
+                      <li>Passion for Excellence</li>
+                    </ul>
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
-          <div>hello</div>
+        </div>
+        <div className="infrastructure_container">
+          <div className="infrastrure_content">
+            <span>_____________</span>
+            <h1>Our Infrastructure Expertise</h1>
+            <div className="flex_infrastructure">
+              <div className="grid_infrastructure_col">
+                <div className="infra_col">
+                  <div className="infra_col_content">
+                    <span>
+                      <img src="" alt="" />
+                    </span>
+                    <span>
+                      <h3>We develp</h3>
+                    </span>
+                  </div>
+                </div>
+                <div className="infra_col">
+                  <div className="infra_col_content">
+                    <span>
+                      <img src="" alt="" />
+                    </span>
+                    <span>
+                      <h3>We develp</h3>
+                    </span>
+                  </div>
+                </div>
+                <div className="infra_col">
+                  <div className="infra_col_content">
+                    <img src="" alt="img" />
+                    <h3>We develp</h3>
+                    <p>
+                      We DELIVER Our exceptional manufacturing and testing
+                      facilities enable us to cater to different industry needs
+                      and the constantly changing demands of the market.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="explore_segment_container">
+          <h2>EXPLORE OUR SEGMENTS</h2>
+          <div className="explore_seg_grid">
+            <div className="grid_explore_col">
+              <span>Automotive</span>
+            </div>
+            <div className="grid_explore_col">
+              <span>Automotive</span>
+            </div>
+            <div className="grid_explore_col">
+              <span className="automotive_text">Automotive</span>
+            </div>
+          </div>
+        </div>
+        <div className="history_container">
+          <div className="history_content">
+            <span>_______________</span>
+            <h1 className="history_h1">HISTORY & MILESTONES</h1>
+          </div>
+        </div>
+        <div className="leadership_container">
+          <div className="leaderhip_content">
+            <span>_______________</span>
+            <h1>Leadership</h1>
+          </div>
+          <div className="leader_flex_content">
+            <div className="leadership_bagwe_img"></div>
+            <div className="leadership_bagwe_content">
+              <div className="leadership_flex_detail">
+                <span>_______________</span>
+                <div>
+                  <h3>Mr. Rajendra Bagwe</h3>
+                  <h4>Co-Founder & Director</h4>
+                </div>
+                <p className="leadership_p">
+                  As a Co-founder and a whole-time Director, Mr. Rajendra Bagwe
+                  helps evaluate new business opportunities, provides strategic
+                  direction and helps construct a robust strategy execution
+                  system at Reliable. With a degree in Mechanical Engineering
+                  from VJTI, Mr. Bagwe has experience working in the R&D
+                  division of Mahindra & Mahindra for four years. Adding to his
+                  knowledge background, he has also studied Executive Education
+                  courses from IIM Ahmedabad and a three-year Owner President
+                  Management Program at Harvard Business School. He is the
+                  founder and chief mentor at the Teaching Learning Community
+                  (TLC), a group of over 700 entrepreneurs who have come
+                  together to learn & share from each other’s experiences,
+                  knowledge, and best practices.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="leader_flex_content">
+            <div className="leadership_bagwe_img"></div>
+            <div className="leadership_bagwe_content">
+              <div className="leadership_flex_detail">
+                <span>_______________</span>
+                <div>
+                  <h3>Mr. Rajendra Bagwe</h3>
+                  <h4>Co-Founder & Director</h4>
+                </div>
+                <p className="leadership_p">
+                  As a Co-founder and a whole-time Director, Mr. Rajendra Bagwe
+                  helps evaluate new business opportunities, provides strategic
+                  direction and helps construct a robust strategy execution
+                  system at Reliable. With a degree in Mechanical Engineering
+                  from VJTI, Mr. Bagwe has experience working in the R&D
+                  division of Mahindra & Mahindra for four years. Adding to his
+                  knowledge background, he has also studied Executive Education
+                  courses from IIM Ahmedabad and a three-year Owner President
+                  Management Program at Harvard Business School. He is the
+                  founder and chief mentor at the Teaching Learning Community
+                  (TLC), a group of over 700 entrepreneurs who have come
+                  together to learn & share from each other’s experiences,
+                  knowledge, and best practices.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="leader_flex_content">
+            <div className="leadership_bagwe_img"></div>
+            <div className="leadership_bagwe_content">
+              <div className="leadership_flex_detail">
+                <span>_______________</span>
+                <div>
+                  <h3>Mr. Rajendra Bagwe</h3>
+                  <h4>Co-Founder & Director</h4>
+                </div>
+                <p className="leadership_p">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Ullam placeat ducimus beatae accusantium excepturi facere
+                  fugit, magni, hic assumenda velit accusamus nulla itaque,
+                  totam voluptas ipsum. Consequuntur necessitatibus modi quo.
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Ullam placeat ducimus beatae accusantium excepturi facere
+                  fugit, magni, hic assumenda velit accusamus nulla itaque,
+                  totam voluptas ipsum. Consequuntur necessitatibus modi quo.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="leader_flex_content">
+            <div className="leadership_bagwe_img"></div>
+            <div className="leadership_bagwe_content">
+              <div className="leadership_flex_detail">
+                <span>_______________</span>
+                <div>
+                  <h3>Mr. Rajendra Bagwe</h3>
+                  <h4>Co-Founder & Director</h4>
+                </div>
+                <p className="leadership_p">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Ullam placeat ducimus beatae accusantium excepturi facere
+                  fugit, magni, hic assumenda velit accusamus nulla itaque,
+                  totam voluptas ipsum. Consequuntur necessitatibus modi quo.
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Ullam placeat ducimus beatae accusantium excepturi facere
+                  fugit, magni, hic assumenda velit accusamus nulla itaque,
+                  totam voluptas ipsum. Consequuntur necessitatibus modi quo.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="leader_button">
+          <button className="leader_btn">Join The Reliable Team</button>
+          <button className="leader_btn">Join The Reliable Team</button>
         </div>
       </section>
+      <div className="footer_container">
+        <div className="flex_footer">
+          <div className="left_footer_link">
+            <div className="footer_first_row_links">
+              <span>Company</span>
+              <span>|</span>
+              <span>Customers</span>
+              <span>|</span>
+              <span>Infrastructure & Facilities</span>
+              <span>|</span>
+              <span>Processes & Certification</span>
+            </div>
+
+            <div className="footer_first_second_links">
+              <span>Location</span>
+              <span>|</span>
+              <span>CSR</span>
+              <span>|</span>
+              <span>People</span>
+              <span>|</span>
+              <span>Downloads</span>
+              <span>|</span>
+              <span>Investors</span>
+              <span>|</span>
+              <span>Carrers</span>
+              <span>|</span>
+              <span>Contact</span>
+            </div>
+          </div>
+          <div className="right_follows_footer">
+            <div className="right_follows_footer_content">
+              <span>FOLLOW US</span>
+              <div className="footer_content_link">
+                <span>In</span>
+                <span>Facebook</span>
+                <span>Youtube</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="footer_about">
+          <div>
+            <p>
+              © 2023 by Reliable Autotech Private Limited. All Rights Reserved.
+            </p>
+            <p>Designed by 360Enrich.com, Comet Studio & Priyanka Mehta.</p>
+            <p className="footer_p_tag">
+              <small>
+                “Reliable Autotech” and the Reliable Logo are registered®
+                trademarks of Reliable Autotech Private Limited. Any
+                unauthorised use is strictly prohibited. All product and company
+                names are trademarks™ or registered® trademarks of their
+                respective holders. Use of them does not imply any affiliation
+                with or endorsement by them.
+              </small>
+            </p>
+          </div>
+          <div className="right_footer_policy">
+            <span>privacy Policy </span>
+            <span>|</span>
+            <span>SiteMap</span>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
